a2a7a36e30f41b4a9b72767ac0b1836c45315afe
Kevin Orr <kevinorr54@gmail.com>
2020-11-13 04:30:46 -0500

    Change order of linker args??

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ from pathlib import Path
 TEST_DIR = Path(__file__).parent
 
 def test_hello_world(tmp_path):
-    subprocess.check_call(['cppmm', str(TEST_DIR/'hello_world/main.cpp'), '-o', str(tmp_path/'main.c')])
-    subprocess.check_call(['gcc', '-lstdc++', str(tmp_path/'main.c'), '-o', str(tmp_path/'a.out')])
+    subprocess.check_call(['cppmm', '-o', str(tmp_path/'main.c'), str(TEST_DIR/'hello_world/main.cpp')])
+    subprocess.check_call(['gcc', '-o', str(tmp_path/'a.out'), str(tmp_path/'main.c'), '-lstdc++'])
     assert subprocess.check_output([str(tmp_path/'a.out')], text=True) \
         == 'Hello, world!\n'
