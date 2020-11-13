357c84fe0412854969b3b148bc755970a38cc080
Kevin Orr <kevinorr54@gmail.com>
2020-11-13 03:16:46 -0500

    Update tests

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import subprocess
-import sys
-
 import pytest
 
+import subprocess
+from pathlib import Path
+
+TEST_DIR = Path(__file__).parent
+
 def test_hello_world(tmp_path):
-    subprocess.check_call([sys.executable, './cppmm.py', 'hello_world/main.cpp', '-o', tmp_path/'main.c'])
-    subprocess.check_call(['g++', tmp_path/'main.c', tmp_path/'a.out'])
-    assert subprocess.check_output([tmp_path/'a.out']) == 'Hello, world!\n'
+    subprocess.check_call(['cppmm', TEST_DIR/'hello_world/main.cpp', '-o', tmp_path/'main.c'])
+    subprocess.check_call(['g++', tmp_path/'main.c', '-o', tmp_path/'a.out'])
+    assert subprocess.check_output([tmp_path/'a.out']) == b'Hello, world!\n'
