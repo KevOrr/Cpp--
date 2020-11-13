10dc11b5dc123a53e6cdca90279f5b323ec7a118
Kevin Orr <kevinorr54@gmail.com>
2020-11-13 04:00:00 -0500

    Until python 3.8, Popen can't take Path-like objects on Windows

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,6 +8,6 @@ from pathlib import Path
 TEST_DIR = Path(__file__).parent
 
 def test_hello_world(tmp_path):
-    subprocess.check_call(['cppmm', TEST_DIR/'hello_world/main.cpp', '-o', tmp_path/'main.c'])
-    subprocess.check_call(['g++', tmp_path/'main.c', '-o', tmp_path/'a.out'])
-    assert subprocess.check_output([tmp_path/'a.out']) == b'Hello, world!\n'
+    subprocess.check_call(['cppmm', str(TEST_DIR/'hello_world/main.cpp'), '-o', str(tmp_path/'main.c')])
+    subprocess.check_call(['g++', str(tmp_path/'main.c'), '-o', str(tmp_path/'a.out')])
+    assert subprocess.check_output([str(tmp_path/'a.out')]) == b'Hello, world!\n'
